681a423eed71760de5dea8d8be02e2d29b3c6def
JAYABALAJEE S K GitHub <tharunbalajee04@gmail.com>
2025-04-16 11:36:27 +0530

    Update Employee By Id - REST API - Employee: Service, Implementation, Controller

--- ems-backend/src/main/java/com/jb/ems_backend/service/impl/EmployeeServiceImpl.java
+++ ems-backend/src/main/java/com/jb/ems_backend/service/impl/EmployeeServiceImpl.java
@@ -43,5 +43,16 @@ public class EmployeeServiceImpl implements EmployeeService {
                 .map((employee) -> EmployeeMapper.mapToEmployeeDto(employee))
                 .collect(Collectors.toList());
     }
+
+    @Override
+    public EmployeeDto updateEmployee(Long employeeId, EmployeeDto updatedEmployee) 
+    {
+        Employee employee = employeeRepository.findById(employeeId).orElseThrow(() -> new ResourceNotFoundException("Employee is not exist with given id: " + employeeId));
+        employee.setFirstName(updatedEmployee.getFirstName());
+        employee.setLastName(updatedEmployee.getLastName());
+        employee.setEmail(updatedEmployee.getEmail());
+        Employee updatedEmployeeObj = employeeRepository.save(employee);
+        return EmployeeMapper.mapToEmployeeDto(updatedEmployeeObj);
+    }
     
 }
