7e9879a05c75a8bdee0eb20240459500ac19c738
JAYABALAJEE S K GitHub <tharunbalajee04@gmail.com>
2025-05-06 10:37:39 +0530

    Simple Changes

--- expense-tracker/src/main/java/com/jb/expense_tracker/repository/CategoryRepository.java
+++ expense-tracker/src/main/java/com/jb/expense_tracker/repository/CategoryRepository.java
@@ -1,7 +1,5 @@
 package com.jb.expense_tracker.repository;
-
 import java.util.Locale.Category;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
