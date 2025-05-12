1e22271fcf97a052c25dc298185167a18153ab38
JAYABALAJEE S K GitHub <tharunbalajee04@gmail.com>
2025-05-12 14:44:45 +0530

    First Spring Boot Project - Demo

---
+++ spring-boot-first-app/src/main/java/com/jb/spring_boot_first_app/SpringBootFirstAppApplication.java
@@ -0,0 +1,13 @@
+package com.jb.spring_boot_first_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFirstAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFirstAppApplication.class, args);
+	}
+
+}
