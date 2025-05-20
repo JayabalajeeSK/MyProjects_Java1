0decec80a0d83714aad1b4ae005ed9d9b3e1beff
JAYABALAJEE S K GitHub <tharunbalajee04@gmail.com>
2025-05-20 18:43:31 +0530

    Deployment

---
+++ MyPro/banking-system-application/src/test/java/com/jb/banking_system_application/BankingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jb.banking_system_application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
