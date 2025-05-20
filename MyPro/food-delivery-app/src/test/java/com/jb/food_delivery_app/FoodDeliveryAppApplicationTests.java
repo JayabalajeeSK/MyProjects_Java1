561c48b01affe245bca66f797c2d2c6a18903204
JAYABALAJEE S K GitHub <tharunbalajee04@gmail.com>
2025-05-20 18:49:27 +0530

    Deployment

---
+++ MyPro/food-delivery-app/src/test/java/com/jb/food_delivery_app/FoodDeliveryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jb.food_delivery_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDeliveryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
