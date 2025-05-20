893a42ae5e892452cf9d75f32a85e12f89fbd44a
JAYABALAJEE S K GitHub <tharunbalajee04@gmail.com>
2025-05-20 18:40:35 +0530

    Deployment

---
+++ MyPro/health-care-appointment-app/src/main/java/com/jb/health_care_appointment_app/HealthCareAppointmentAppApplication.java
@@ -0,0 +1,13 @@
+package com.jb.health_care_appointment_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthCareAppointmentAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthCareAppointmentAppApplication.class, args);
+	}
+
+}
---
+++ MyPro/health-care-appointment-app/src/test/java/com/jb/health_care_appointment_app/HealthCareAppointmentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jb.health_care_appointment_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthCareAppointmentAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
