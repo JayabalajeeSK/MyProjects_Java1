9fbbffa5d1a73f861094e4ff38fb07d74926b745
JAYABALAJEE S K GitHub <tharunbalajee04@gmail.com>
2025-05-29 11:06:22 +0530

    Quizzes Entity - id, title, desc, durMin, maxAttempts, List of Questions

---
+++ online-quiz-app/src/main/java/com/jb/online_quiz_app/entity/Quiz.java
@@ -0,0 +1,41 @@
+package com.jb.online_quiz_app.entity;
+
+import java.util.List;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "quizzes")
+public class Quiz 
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @Column(length = 1000)
+    private String description;
+
+    private int durationMinutes;
+
+    private int maxAttempts;
+
+    // Bi-directional (optional)
+    @OneToMany(mappedBy = "quiz", cascade = CascadeType.ALL)
+    private List<Question> questions;
+}
