d3539f5df3cb0da5898c471d5c03bea8ff80e534
JAYABALAJEE S K GitHub <tharunbalajee04@gmail.com>
2025-05-29 11:23:37 +0530

    id, user id (many to one - many attempts by one user), quiz id (many to one - many attempts on one / same quiz), startTime, endTime, [localdatetime], score (int), submitted (boolean - 1/0 - y/n), list of student answer (one to many - one attempt by multiple student answer)

---
+++ online-quiz-app/src/main/java/com/jb/online_quiz_app/entity/QuizAttempt.java
@@ -0,0 +1,49 @@
+package com.jb.online_quiz_app.entity;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "quiz_attempts")
+public class QuizAttempt 
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne //many attempts by one user
+    @JoinColumn(name="user_id")
+    private User user;
+
+    @ManyToOne //many attempts on one / same quiz
+    @JoinColumn(name="quiz_id")
+    private Quiz quiz;
+
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+
+    private int score;
+
+    private boolean submitted;
+
+    @OneToMany(mappedBy = "attempt", cascade = CascadeType.ALL) //one attempt by multiple student answer
+    private List<StudentAnswer> answers;
+}
