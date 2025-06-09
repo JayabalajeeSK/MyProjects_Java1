a9e41dedeb3437f41870f39cc8068a4f4073efd4
JAYABALAJEE S K GitHub <tharunbalajee04@gmail.com>
2025-06-09 16:59:14 +0530

    Connected Delete Employee React App to Update REST API – Backend

--- full_stack_pro/ems-frontend/src/components/ListEmployeeComponent.jsx
+++ full_stack_pro/ems-frontend/src/components/ListEmployeeComponent.jsx
@@ -1,6 +1,5 @@
-import React, { useEffect } from 'react'
-import { useState } from 'react'
-import { listEmployees } from '../services/EmployeeService'
+import React, { useEffect, useState } from 'react'
+import { listEmployees, deleteEmployee } from '../services/EmployeeService'
 import { useNavigate } from 'react-router-dom'
 
 const ListEmployeeComponent = () => {
@@ -8,58 +7,67 @@ const ListEmployeeComponent = () => {
     const [employee, setEmployees] = useState([])
     const navigator = useNavigate(); 
 
-    useEffect(() => 
-    {
-        listEmployees().then((response) => 
-        {
-            setEmployees(response.data);
-        }).catch(error => console.error(error));
-    }, [] ) 
+    useEffect(() => {
+        getAllEmployees();
+    }, []);
 
-    function addNewEmployee()
-    {
+    function getAllEmployees() {
+        listEmployees().then((response) => {
+            setEmployees(response.data);
+        }).catch(error => {
+            console.error(error)
+        });
+    }
+
+    function addNewEmployee() {
         navigator("/add-employee")
     }
 
-    function updateEmployee(id)
-    {
+    function updateEmployee(id) {
         navigator(`/edit-employee/${id}`)
     }
 
-  return (
-    <div className='container'>
-        <h2 className='text-center'> List Of Employees </h2>
-        <button type="button" class="btn btn-primary mb-2" onClick={addNewEmployee}>Add Employee</button>
-        <table class="table table-striped table-hover">
-            <thead>
-                <tr>
-                    <th> Employee ID</th>
-                    <th> Employee First Name</th>
-                    <th> Employee Last Name</th>
-                    <th> Employee Email ID</th>
-                    <th> Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                {
-                    employee.map(employee => 
-                        <tr key={employee.id}>
+    function removeEmployee(id) {
+        console.log(id);
+        deleteEmployee(id).then((response) => {
+            getAllEmployees();
+        }).catch(error => {
+            console.error(error);
+        })
+    }
 
-                            <td>{employee.id}</td>
-                            <td>{employee.firstName}</td>
-                            <td>{employee.lastName}</td>
-                            <td>{employee.email}</td>
-                            <td>
-                                <button className='btn btn-info' onClick={() => updateEmployee(employee.id)}>Update</button>
-                            </td>
-
-                        </tr>
-                    )
-                }
-            </tbody>
-        </table>
-    </div>
-  )
+    return (
+        <div className='container'>
+            <h2 className='text-center'> List Of Employees </h2>
+            <button type="button" className="btn btn-primary mb-2" onClick={addNewEmployee}>Add Employee</button>
+            <table className="table table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th>Employee ID</th>
+                        <th>Employee First Name</th>
+                        <th>Employee Last Name</th>
+                        <th>Employee Email ID</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        employee.map(emp => 
+                            <tr key={emp.id}>
+                                <td>{emp.id}</td>
+                                <td>{emp.firstName}</td>
+                                <td>{emp.lastName}</td>
+                                <td>{emp.email}</td>
+                                <td>
+                                    <button className='btn btn-info me-2' onClick={() => updateEmployee(emp.id)}>Update</button>    
+                                    <button className='btn btn-danger' onClick={() => removeEmployee(emp.id)}>Delete</button>
+                                </td>
+                            </tr>
+                        )
+                    }
+                </tbody>
+            </table>
+        </div>
+    )
 }
-
 export default ListEmployeeComponent
